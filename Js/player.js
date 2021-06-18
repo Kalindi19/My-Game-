19389fb3616ac9ee9e4fecc0b12c4539e3fb4137
Kalindi <78595946+Kalindi19@users.noreply.github.com>
2021-06-18 17:01:38 +0530

    Add files via upload

---
+++ Js/player.js
@@ -0,0 +1,49 @@
+class Player{
+    constructor(){
+        this.index=null;
+        this.distance=0;
+        this.name=null;
+        this.rank=null;
+    }
+    getCount(){
+        database.ref("playerCount").on("value",function (data){
+            playerCount=data.val();
+        })
+    }
+
+    updateCount(count){
+        database.ref("/").update({
+            playerCount:count
+        })
+    }
+
+    update(){
+        database.ref("players"+this.index).set({
+            name:this.name,
+            distance:this.distance
+        
+        })
+    }
+
+    static getPlayerinfo(){
+        database.ref('players').on("value",(data)=>{
+            allPlayers=data.val();
+        })
+    }
+
+    getCarsAtEnd(){
+        database.ref('carsAtEnd').on("value",(data)=>{
+            this.rank=data.val();
+        })
+
+    }
+
+    static updateCarsAtEnd(rank){
+        database.ref('/').update({
+            carsAtEnd:rank
+        })
+
+    }
+
+
+}
